f51926b770afa8ccd9ad45fb39bd270781fb3302
Hardik Poriya <hardikkumar.poriya@gmail.com>
2022-03-13 18:05:04 +0530

    initial commit

---
+++ Test.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Test{
+
+	public static void main(String[] args){
+		int[] d = new int[]{1,2,20,8,8,1,2,5,8,0};
+		List<Integer> elements = Arrays.asList( 1,2,20,8,8,1,2,5,8,0);
+		List<Integer> queries = Arrays.asList(100,4,2);
+		List<Integer> output = new ArrayList<>();
+		List<Integer> temp = new ArrayList<>();
+		int x=8;
+		for(int i=0;i<elements.size();i++){
+			if(elements.get(i)== x){
+				temp.add(i+1);
+			}
+		}
+		System.out.println(elements);
+		System.out.println(temp);
+		
+		List<Integer> list = new ArrayList<Integer>();
+		for(int i=0;i<queries.size();i++){
+			try{
+				System.out.println(queries.get(i)-1);
+				
+				list.add(temp.get(queries.get(i)-1));
+			} catch (Exception e){
+				list.add(-1);
+			}
+							
+		}
+		System.out.println(list);
+	}
+
+}
